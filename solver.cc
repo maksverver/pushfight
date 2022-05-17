c2d89d6464c3706e0a898556c9071b21f5c4e413
Maks Verver <maks@verver.ch>
2022-05-17 22:43:34 +0200

    Add logic to calculate pass 0.

--- solver.cc
+++ solver.cc
@@ -2,12 +2,21 @@
 #include "perms.h"
 
 #include <cassert>
+#include <chrono>
+#include <fstream>
+#include <iomanip>
 #include <iostream>
+#include <sstream>
+#include <filesystem>
 #include <functional>
 #include <string>
+#include <thread>
 #include <vector>
 
 namespace {
+
+// Number of threads to use for calculations. 0 to disable multithreading.
+constexpr int num_threads = 8;
 
 constexpr int H = 4;
 constexpr int W = 8;
@@ -279,51 +288,96 @@ bool IsBlackInDanger(const Perm &p) {
   return false;
 }
 
+constexpr int chunk_size = 54054000;
+constexpr int num_chunks = 7429;
+static_assert(int64_t{chunk_size} * int64_t{num_chunks} == total_perms);
+
+Outcome CalculateR0(const Perm &perm) {
+  // Optimization: for the first pass, only check successors if there is a
+  // pushable black piece on an edge spot. Otherwise, we cannot win this turn.
+  // Note: this means we cannot detect losses due to being unable to move.
+  // My hypothesis is that there are no such positions, but if there are, the
+  // we should interpret TIE as "not a WIN (possibly a loss)".
+  if (!IsBlackInDanger(perm)) return TIE;
+
+  Outcome o = TIE;
+  int n = 0;
+  bool aborted = GenerateSuccessors(perm, [&o, &n](const Moves &moves, const State &state) {
+    (void) moves;  // unused
+    ++n;
+    if (state.outcome == LOSS) {
+      o = WIN;
+      return false;
+    }
+    return true;
+  });
+  if (n == 0) {
+    Dump(perm, std::cout);
+  }
+  assert(n > 0);  // hypothesis: there is always some valid move
+  return o;
+}
+
+void ComputeChunkThread(int64_t offset, int size, Outcome *outcomes) {
+  Perm perm = PermAtIndex(offset);
+  REP(i, size) {
+    *outcomes++ = CalculateR0(perm);
+    std::next_permutation(perm.begin(), perm.end());
+  }
+}
+
+std::vector<Outcome> ComputeChunk(int chunk) {
+  std::vector<uint8_t> bits(chunk_size / 8);
+  int64_t start_index = int64_t{chunk} * int64_t{chunk_size};
+  std::vector<Outcome> outcomes(chunk_size, TIE);
+  if (num_threads == 0) {
+    // Single-threaded computation.
+    ComputeChunkThread(start_index, chunk_size, outcomes.data());
+  } else {
+    assert(chunk_size % num_threads == 0);
+    // Multi-threaded computation.
+    std::vector<std::thread> threads;
+    const int thread_size = chunk_size / num_threads;
+    REP(i, num_threads) {
+      threads.emplace_back(ComputeChunkThread, start_index + thread_size * i, thread_size,
+          outcomes.data() + thread_size * i);
+    }
+    REP(i, num_threads) threads[i].join();
+  }
+  return outcomes;
+}
+
+std::string FileName(int chunk) {
+  std::ostringstream oss;
+  oss << "chunk-r0-" << std::setfill('0') << std::setw(4) << chunk << ".bin";
+  return oss.str();
+}
+
+void ProcessChunk(int chunk) {
+  std::vector<Outcome> outcomes = ComputeChunk(chunk);
+  assert(outcomes.size() % 8 == 0);
+  std::vector<uint8_t> bytes(outcomes.size() / 8, uint8_t{0});
+  REP(i, outcomes.size()) {
+    assert(outcomes[i] == TIE || outcomes[i] == WIN);
+    if (outcomes[i] == WIN) bytes[i / 8] |= 1 << (i % 8);
+  }
+  std::ofstream os(FileName(chunk), std::fstream::binary);
+  os.write(reinterpret_cast<char*>(bytes.data()), bytes.size());
+}
+
 }  // namespace
 
 int main() {
   InitializePerms();
-  Perm perm = initial_state;
-
-  // Results for the first 1,000,000 permutations:
-  //
-  //  Losses: 0
-  //  Ties:   398867
-  //  Wins:   601133
-  //
-  // Time taken: 21s (47,619 permutations/second)
-
-  int64_t outcome_counts[3] = {0, 0, 0};
-  REP(_, 1000000) {
-    //Dump(perm, std::cout) << std::endl;
-    Outcome o = TIE;
-    // Optimization: for the first pass, only check successors if there is a
-    // pushable black piece on an edge spot. Otherwise, we cannot win this turn.
-    // Note: this means we cannot detect losses due to being unable to move.
-    // My hypothesis is that there are no such positions, but if there are, the
-    // we should interpret TIE as "not a WIN (possibly a loss)".
-    if (IsBlackInDanger(perm)) {
-      int n = 0;
-      GenerateSuccessors(perm, [&o, &n](const Moves &moves, const State &state) {
-        (void) moves;  // unused
-        ++n;
-        o = MaxOutcome(o, INVERSE_OUTCOME[state.outcome]);
-        if (o == WIN) {
-          /*
-          const Moves &moves = successor.first;
-          const State &state = successor.second;
-          Dump(moves, std::cout) << std::endl;
-          Dump(state, std::cout) << std::endl;
-          */
-        }
-        return o != WIN;
-      });
-      assert(n > 0);  // hypothesis: there is always some valid move
+  REP(chunk, num_chunks) {
+    if (std::filesystem::exists(FileName(chunk))) {
+      std::cerr << "Chunk " << chunk << " already exists. Skipping..." << std::endl;
+      continue;
     }
-    outcome_counts[o]++;
-    std::next_permutation(perm.begin(), perm.end());
+    std::cerr << "Chunk " << chunk << " calculating... " << std::flush;
+    auto start_time = std::chrono::system_clock::now();
+    ProcessChunk(chunk);
+    std::chrono::duration<double> elapsed_seconds = std::chrono::system_clock::now() - start_time;
+    std::cerr << "Done in " << elapsed_seconds.count() / 60 << " minutes." << std::endl;
   }
-  std::cout << "Losses: " << outcome_counts[LOSS] << '\n';
-  std::cout << "Ties:   " << outcome_counts[TIE] << '\n';
-  std::cout << "Wins:   " << outcome_counts[WIN] << '\n';
 }
