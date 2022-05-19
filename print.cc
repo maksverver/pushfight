d2398f474aabea4320b961d62a8605618ceb72d2
Maks Verver <maks@verver.ch>
2022-05-19 22:41:36 +0200

    Add an option to analyze a permutation given as a 26-character string.

--- print.cc
+++ print.cc
@@ -7,7 +7,9 @@
 #include <iostream>
 #include <sstream>
 
-int64_t ParseInt(const char *s) {
+namespace {
+
+int64_t ParseInt64(const char *s) {
   std::istringstream iss(s);
   int64_t index = -1;
   iss >> index;
@@ -15,24 +17,56 @@ int64_t ParseInt(const char *s) {
   return index;
 }
 
+void DumpPerm(const Perm &perm) {
+  Dump(perm, std::cout) << std::endl;
+
+  Outcome o = LOSS;
+  GenerateSuccessors(perm, [&o](const Moves &moves, const State &state) {
+    Dump(moves, std::cout) << std::endl;
+    Dump(state, std::cout) << std::endl;
+    o = MaxOutcome(o, INVERSE_OUTCOME[state.outcome]);
+    return true;
+  });
+  std::cout << "Verdict: " << (o == WIN ? "win" : o == LOSS ? "loss" : "tie") << std::endl;
+}
+
+int count(const std::string &s, char target) {
+  int n = 0;
+  for (char ch : s) if (ch == target) ++n;
+  return n;
+}
+
+}  // namespace
+
 int main(int argc, char *argv[]) {
   InitializePerms();
 
-  if (argc == 3 && std::string(argv[1]) == "by-index") {
-    int64_t index = ParseInt(argv[2]);
-    Perm perm = PermAtIndex(index);
-    Dump(perm, std::cout) << std::endl;
+  const std::string argv1 = argc > 1 ? argv[1] : "";
 
-    Outcome o = LOSS;
-    GenerateSuccessors(perm, [&o](const Moves &moves, const State &state) {
-      Dump(moves, std::cout) << std::endl;
-      Dump(state, std::cout) << std::endl;
-      o = MaxOutcome(o, INVERSE_OUTCOME[state.outcome]);
-      return true;
-    });
-    std::cout << "Verdict: " << (o == WIN ? "win" : o == LOSS ? "loss" : "tie") << std::endl;
+  if (argc == 3 && argv1 == "by-index") {
+    int64_t index = ParseInt64(argv[2]);
+    Perm perm = PermAtIndex(index);
+      DumpPerm(perm);
+  } else if (argc == 3 && argv1 == "by-perm") {
+    std::string argv2 = argv[2];
+    if (argv2.size() != 26) {
+      std::cout << "Invalid length! Expected 26." << std::endl;
+    } else if (count(argv2, 'o') != 2 || count(argv2, 'O') != 3 || count(argv2, 'x') != 2
+        || count(argv2, 'X') != 2 || count(argv2, 'Y') != 1) {
+      std::cout << "Invalid character counts." << std::endl;
+    } else {
+      Perm perm;
+      REP(i, 26) {
+        char ch = argv2[i];
+        perm[i] = ch == 'o' ? WHITE_MOVER : ch == 'O' ? WHITE_PUSHER : ch == 'x' ? BLACK_MOVER :
+            ch == 'X' ? BLACK_PUSHER : ch == 'Y' ? BLACK_ANCHOR : EMPTY;
+      }
+      std::cout << "Permutation index: " << IndexOf(perm) << std::endl;
+      DumpPerm(perm);
+    }
   } else {
     std::cout << "Usage:\n" <<
-        "  print by-index 123\n";
+        "  print by-index 123\n"
+        "  print by-perm .OX.....oxY....Oox.....OX.\n";
   }
 }
