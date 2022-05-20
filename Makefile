a9ba175952b7d63caeb1edf135a9bd1a41dc2ddf
Maks Verver <maks@verver.ch>
2022-05-21 00:02:25 +0200

    Add a tool to prove there are no immediately-losing positions.
    i.e., it is always possible to make a valid push by the end of the turn. If proven true,
    that means that a player can only lose by one of their pieces being pushed off the board
    by the opponent.

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ CXXFLAGS=-std=c++20 -Wall -Wextra -Wno-sign-compare -O3 -march=native -flto -pth
 LDLIBS=-lpthread -lm
 
 COMMON_OBJS=accessors.o perms.o board.o chunks.o search.o
-BINARIES=countbits print-perm solve-r0 solve-r1 solve-rN verify-r0 print-r1
+BINARIES=countbits print-perm solve-r0 solve-r1 solve-rN solve-lost verify-r0 print-r1
 TESTS=perms_test
 
 all: $(BINARIES) $(TESTS)
@@ -32,6 +32,9 @@ solve-r1: solve-r1.cc $(COMMON_OBJS)
 	$(CXX) $(CXXFLAGS) -o $@ $^ $(LDLIBS)
 
 solve-rN: solve-rN.cc $(COMMON_OBJS)
+	$(CXX) $(CXXFLAGS) -o $@ $^ $(LDLIBS)
+
+solve-lost: solve-lost.cc $(COMMON_OBJS)
 	$(CXX) $(CXXFLAGS) -o $@ $^ $(LDLIBS)
 
 print-perm: print-perm.cc $(COMMON_OBJS)
---
+++ solve-lost.cc
@@ -0,0 +1,46 @@
+// This solver tries to find positions that are losing because the next player
+// has no push without pushing his own pieces off the board.
+//
+// My hypothesis is that no such positions exist.
+
+#include "board.h"
+#include "perms.h"
+#include "search.h"
+
+#include <algorithm>
+#include <chrono>
+#include <iostream>
+
+int main() {
+  InitializePerms();
+  Perm perm = first_perm;
+  int64_t num_found = 0;
+  int64_t index = 0;
+  constexpr int reporting_interval = 1 << 30;
+  auto start_time = std::chrono::system_clock::now();
+  do {
+    bool complete = GenerateSuccessors(perm, [](const Moves &moves, const State &state) {
+      (void) moves;  // unused
+      // Skip states where player pushes their own piece off the board.
+      // Abort the search in any other case.
+      return state.outcome == WIN;
+    });
+    if (complete) {
+      ++num_found;
+      std::cout << "Found losing permutation: " << index << std::endl;
+      Dump(perm, std::cout);
+    }
+    ++index;
+    if (index % reporting_interval == 0) {
+      // Print progress as a percentage, and estimated time remaining in minutes.
+      auto end_time = std::chrono::system_clock::now();
+      std::chrono::duration<double> elapsed_seconds = end_time - start_time;
+      start_time = end_time;
+      double remaining_seconds = (total_perms - index) * elapsed_seconds.count() / reporting_interval;
+      std::cerr << 100.0 * index / total_perms << "% complete. "
+          << "Estimated time remaining: " << remaining_seconds / 60 << " minutes."
+          << std::endl;
+    }
+  } while (std::next_permutation(perm.begin(), perm.end()));
+  std::cout << num_found << " / " << index << " permutations are losing. " << std::endl;
+}
