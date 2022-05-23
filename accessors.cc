45992eb92a978c84a9b648bb7dd160c93e5bea30
Maks Verver <maks@verver.ch>
2022-05-23 13:27:31 +0200

    More robust checking of input file before mmap().

--- accessors.cc
+++ accessors.cc
@@ -7,7 +7,33 @@
 #include <sys/mman.h>
 #include <unistd.h>
 
+#include <filesystem>
+
+namespace {
+
+void CheckFileSize(const char *filename, size_t size) {
+  if (!std::filesystem::exists(filename)) {
+    std::cerr << "File " << filename << " does not exist.\n";
+    exit(1);
+  }
+  auto filesize = std::filesystem::file_size(filename);
+  if (filesize < size) {
+    std::cerr << "File " << filename << " is too short. "
+        "Expected " << size << " bytes, actual " << filesize << " bytes.\n";
+    exit(1);
+  }
+  if (filesize > size) {
+    std::cerr << "WARNING: file " << filename << " is too long. "
+        "Expected " << size << " bytes, actual " << filesize << " bytes.\n";
+    // Don't exit. We should still be able to map the prefix.
+  }
+}
+
+}  // namespace
+
 void *MemMap(const char *filename, size_t length) {
+  CheckFileSize(filename, length);
+
   int fd = open(filename, O_RDONLY);
   if (fd == -1) {
     std::cerr << "Failed to open() " << filename << std::endl;
@@ -15,7 +41,7 @@ void *MemMap(const char *filename, size_t length) {
   }
   void *data = mmap(nullptr, length, PROT_READ, MAP_PRIVATE, fd, 0);
   close(fd);
-  if (!data) {
+  if (data == MAP_FAILED) {
     std::cerr << "Failed to mmap() " << filename << std::endl;
     exit(1);
   }
