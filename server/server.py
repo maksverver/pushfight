6b5e3872c81bfa1308621d32c59df246ef5d9c95
Maks Verver <maks@verver.ch>
2022-05-30 16:02:59 +0200

    Support dualstack IPv4/IPv6 serving.

--- server/server.py
+++ server/server.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python3
 
+import socket
 import socketserver
 from codec import *
 
 class Server(socketserver.ThreadingMixIn, socketserver.TCPServer):
   allow_reuse_address = True
+
+  # Bind on an IPv6 address. This should also work with IPv4 on dualstack
+  # systems. To listen only on IPv4, change the value to AF_INET.
+  address_family = socket.AF_INET6
 
 
 class RequestHandler(socketserver.BaseRequestHandler):
@@ -23,5 +28,7 @@ class RequestHandler(socketserver.BaseRequestHandler):
 
 
 if __name__ == "__main__":
-  server = Server(('localhost', 7429), RequestHandler)
+  addr = ('', 7429)
+  # addr = ('localhost', 7429)  # for local testing
+  server = Server(addr, RequestHandler)
   server.serve_forever()
