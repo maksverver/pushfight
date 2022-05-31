919519310dc3d54457f941fc1caf2fae609d44c3
Maks Verver <maks@verver.ch>
2022-05-31 03:13:06 +0200

    Fix sockets support for Windows.

--- client/socket.cc
+++ client/socket.cc
@@ -1,5 +1,19 @@
 #include "socket.h"
 
+#if _WIN32
+
+#include <winsock2.h>
+#include <ws2tcpip.h>
+
+static int CallWsaStartup() {
+  WSADATA data;
+  return WSAStartup(MAKEWORD(2, 2), &data);
+}
+
+static int wsa_startup_return_value = CallWsaStartup();
+
+#else  // _WIN32
+
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <unistd.h>
@@ -7,12 +21,16 @@
 #include <arpa/inet.h>
 #include <netinet/in.h>
 
+#endif  // _WIN32
+
 #include <cassert>
 #include <iostream>
 #include <optional>
 #include <string>
 
 namespace {
+
+static_assert(sizeof(uint8_t) == sizeof(char));
 
 std::string AddrInfoToString(const addrinfo *ai) {
   if (ai->ai_family == AF_INET) {
@@ -31,6 +49,10 @@ std::string AddrInfoToString(const addrinfo *ai) {
 }  // namespace
 
 std::optional<Socket> Socket::Connect(const char *hostname, const char *portname) {
+#ifdef _WIN32
+  assert(wsa_startup_return_value == 0);
+#endif
+
   struct addrinfo *ai = nullptr;
   if (getaddrinfo(hostname, portname, nullptr, &ai) != 0) {
     std::cerr << "client: Lookup of hostname " << hostname << " failed!" << std::endl;
@@ -38,7 +60,7 @@ std::optional<Socket> Socket::Connect(const char *hostname, const char *portname
   }
 
   for (struct addrinfo *p = ai; p != nullptr; p = p->ai_next) {
-    int s = socket(ai->ai_family, SOCK_STREAM, 0);
+    int s = socket(ai->ai_family, SOCK_STREAM, IPPROTO_TCP);
     if (s == -1) {
       const char *family = ai->ai_family == AF_INET ? "IPv4" : ai->ai_family == AF_INET6 ? "IPv6" : "UNKNOWN";
       std::cerr << "client: Failed to create " << family << " socket!" << std::endl;
@@ -58,7 +80,7 @@ std::optional<Socket> Socket::Connect(const char *hostname, const char *portname
 
 bool Socket::SendAll(const uint8_t *data, size_t size) {
   while (size > 0) {
-    ssize_t n = send(fd, data, size, 0);
+    ssize_t n = send(fd, reinterpret_cast<const char*>(data), size, 0);
     if (n <= 0) {
       std::cerr << "Failed to sent data over socket!" << std::endl;
       return false;
@@ -70,7 +92,7 @@ bool Socket::SendAll(const uint8_t *data, size_t size) {
 }
 
 ssize_t Socket::Receive(uint8_t *data, size_t len) {
-  return recv(fd, data, len, 0);
+  return recv(fd, reinterpret_cast<char*>(data), len, 0);
 }
 
 ssize_t Socket::ReceiveAll(uint8_t *data, size_t len) {
