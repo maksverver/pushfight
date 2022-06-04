485cbbb27f92e52f7a8c32344bae288beeade204
Maks Verver <maks@verver.ch>
2022-06-04 12:46:44 +0200

    Also print checksums in sha256sum format.

--- server/collect-chunks.sh
+++ server/collect-chunks.sh
@@ -3,6 +3,14 @@
 # Collects the chunks from an rN phase by hardlinking files from the
 # incoming/ subdirectory to the chunks/ subdirectory. Files are named
 # like "chunk-r0-0000.bin".
+#
+# Writes the file SHA256 checksums to standard output (in the same format
+# as sha256sum) which should be redirected to a file (or /dev/null).
+#
+# Example:
+#
+#  ./collect-chunks.sh 5 > chunk-r5.sha25sum
+
 
 set -e -E -o pipefail
 
@@ -18,17 +26,19 @@ sqlite3 chunks.db "SELECT phase,chunk,hex(sha256sum) FROM WorkQueue WHERE phase=
   while read phase chunk sha256sum
   do
     sha256sum=$(echo "$sha256sum" | tr A-Z a-z)
+    filename=$(printf 'chunk-r%d-%04d.bin' "$phase" "$chunk")
     src=incoming/$sha256sum
-    dst=$(printf 'chunks/chunk-r%d-%04d.bin' $phase $chunk)
+    dst=chunks/$filename
     if [ ! -f "$src" ]; then
-      echo "$src missing! Chunk: $chunk"
+      echo "$src missing! Chunk: $chunk" >&2
       exit 1
     fi
     if [ -f "$dst" ]; then
-      echo "$dst already exists. Skipping."
+      echo "$dst already exists. Skipping." >&2
     elif ! ln "$src" "$dst"; then
       echo "Could not link $src to $dst!"
       exit 1
     fi
+    echo "$sha256sum  $filename"
   done
 }
