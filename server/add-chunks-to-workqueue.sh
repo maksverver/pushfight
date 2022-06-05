d2be94f194595a38058beee05f9477e8dd77ca46
Maks Verver <maks@verver.ch>
2022-06-05 12:11:41 +0200

    Undo 3e01f38 for now (see comments for details).

--- server/add-chunks-to-workqueue.sh
+++ server/add-chunks-to-workqueue.sh
@@ -22,9 +22,14 @@ fi
   while read chunk ties losses wins total ; do
     if [ $chunk -ge 0 ]; then
       echo "$chunk $ties" >&2
-      if [ $ties -gt 0 ]; then
+      # Note: we could omit solved chunks (without ties) from the work queue,
+      # but then stitching the results back together becomes more complicated
+      # since we need to merge existing blocks from the previous bin file
+      # instead of simply concatenating the results. Let's not bother with it
+      # while the percentage of solved chunks is only ~10%.
+      #if [ $ties -gt 0 ]; then
         echo "INSERT INTO WorkQueue (phase, chunk, difficulty) VALUES ($phase, $chunk, $ties);"
-      fi
+      #fi
     fi
   done
 
