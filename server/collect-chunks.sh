875dac51b90c5e507bc01b505162def3881cac18
Maks Verver <maks@verver.ch>
2022-06-06 12:04:40 +0200

    Add ability to collect compressed chunks from archive/ subdirectory too.

--- server/collect-chunks.sh
+++ server/collect-chunks.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # Collects the chunks from an rN phase by hardlinking files from the
 # incoming/ subdirectory to the chunks/ subdirectory. Files are named
@@ -13,6 +13,7 @@
 
 
 set -e -E -o pipefail
+shopt -s nullglob
 
 phase=$1
 
@@ -27,18 +28,31 @@ sqlite3 chunks.db "SELECT phase,chunk,hex(sha256sum) FROM WorkQueue WHERE phase=
   do
     sha256sum=$(echo "$sha256sum" | tr A-Z a-z)
     filename=$(printf 'chunk-r%d-%04d.bin' "$phase" "$chunk")
-    src=incoming/$sha256sum
+    echo "$sha256sum  $filename"
     dst=chunks/$filename
-    if [ ! -f "$src" ]; then
-      echo "$src missing! Chunk: $chunk" >&2
-      exit 1
-    fi
     if [ -f "$dst" ]; then
       echo "$dst already exists. Skipping." >&2
-    elif ! ln "$src" "$dst"; then
-      echo "Could not link $src to $dst!"
-      exit 1
+    else
+      src=incoming/$sha256sum
+      if [ -f "$src" ]; then
+        # Link newly uploaded chunk.
+        if ! ln "$src" "$dst"; then
+          echo "Could not link $src to $dst!" >&2
+          exit 1
+        fi
+      else
+        compressed_srcs=(archive/*/$sha256sum.zst)
+        if [ ${#compressed_srcs[@]} -gt 0 ]; then
+          compressed_src=${compressed_srcs[0]}
+          if ! zstdcat "$compressed_src" > "$dst"; then
+            echo "Could not decompress $compressed_src to $dst!" >&2
+            exit 1
+          fi
+        else
+          echo "$src missing! Chunk: $chunk" >&2
+          exit 1
+        fi
+      fi
     fi
-    echo "$sha256sum  $filename"
   done
 }
