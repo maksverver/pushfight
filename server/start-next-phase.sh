ea62cb61f43437b8d913991c706879f3c803de8c
Maks Verver <maks@verver.ch>
2022-06-19 17:02:35 +0200

    Fix bug in input generation logic.

--- server/start-next-phase.sh
+++ server/start-next-phase.sh
@@ -13,8 +13,9 @@ prev_phase=$((phase - 2))
 for file in "../metadata/chunk-r${prev_phase}.sha256sum" "../input/r${prev_phase}-new.bin"; do
   if [ ! -f "$file" ]; then
     echo "Missing input file: $file"
-    ./encode-input-file.py "$file" input/"$(basename "$file")"
+    exit 1
   fi
+  ./encode-input-file.py "$file" input/"$(basename "$file")"
 done
 
 ./add-solve2-chunks-to-workqueue.sh "$phase"
