f2bb65390e3dbc935ef1a7bb12284d0b602f2bea
Maks Verver <maks@verver.ch>
2022-07-15 17:22:39 +0200

    Document lookup-min and define separate exit code for user error.

--- lookup-min.cc
+++ lookup-min.cc
@@ -1,5 +1,49 @@
 // Tool to look up a value at a specific index in a minimized merged file,
 // along with best moves.
+//
+// Usage:
+//
+//  lookup-min <minimized.bin> <permutation>
+//
+// Where <permutation> can be in any format accepted by ParsePerm(), although
+// the tool requires the permutation type to be VALID and not FINISHED.
+//
+// Output format:
+//
+// Multiple lines, one per successor (excluding immediately losing successors,
+// i.e. those where a player pushes their own piece off the board). Each line
+// contains up to three fields separated with spaces. Example:
+//
+//  W1 c3-c4,e3-b2,b2-b3
+//  L1 e3-f3,c3-e3,e2-e3 +55456658050
+//
+// The first field indicates the status of the position. T for tie, W7 for
+// win in 7, L7 for loss in 7, etc. These numbers count the number of turns
+// of the player next to move, so win-in-2 means three more turns will be
+// played: p1, p2, p1 (winning), while loss-in-2 means four more turns:
+// p1, p2, p1, p2 (winning).
+//
+// The second field is the optimal set of moves for the turn. Note that there
+// may be multiple sequences of moves in a turn that lead to the same successor
+// position; only one such sequence is shown per successor.
+//
+// The third field is the minimized index of the successor position. This is
+// present only if the successor is not finished, i.e., it is missing when the
+// status is W1.
+//
+// The lines are ordered from best to worst (i.e., winning moves first,
+// then ties, than losing moves, with winning moves ordered with lowest
+// number of remaining moves first, and losing moves ordered with highest
+// number of remaining moves first), e.g. W1, W2, W3, T, T, L3, L2, L1.
+//
+// Exit status:
+//
+//  0 success
+//  1 internal error (e.g. file not found)
+//  2 client error (e.g. invalid argument)
+//
+// Note that the interface of this tool must be strictly specified because it
+// is used by lookup-min-http-server.py.
 
 #include <cassert>
 #include <iostream>
@@ -68,9 +112,9 @@ std::ostream &operator<<(std::ostream &os, const Value &v) {
 
 int main(int argc, char *argv[]) {
   if (argc != 3) {
-    std::cout <<
+    std::cerr <<
         "Usage: lookup-min <minimized.bin> <permutation>\n";
-    return 0;
+    return 2;
   }
 
   const char *filename = argv[1];
@@ -85,19 +129,19 @@ int main(int argc, char *argv[]) {
     std::string error;
     std::optional<Perm> res = ParsePerm(perm_string, &error);
     if (!res) {
-      std::cout << "Could not parse permutation string \"" << perm_string << "\": "
+      std::cerr << "Could not parse permutation string \"" << perm_string << "\": "
           << error << std::endl;
-      return 1;
+      return 2;
     }
     perm = std::move(*res);
     type = ValidatePerm(perm);
     if (type == PermType::INVALID) {
-      std::cout << "Permutation is invalid!\n";
-      return 1;
+      std::cerr << "Permutation is invalid!\n";
+      return 2;
     }
     if (type == PermType::FINISHED) {
-      std::cout << "Permutation represents a finished position!\n";
-      return 1;
+      std::cerr << "Permutation represents a finished position!\n";
+      return 2;
     }
     assert(type == PermType::STARTED || type == PermType::IN_PROGRESS);
   }
